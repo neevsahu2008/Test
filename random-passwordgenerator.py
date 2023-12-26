893cbf535c7d5af5e703fb47e4ef6d8d5f030b39
Neev sahu <154683415+neevsahu2008@users.noreply.github.com>
2023-12-26 19:52:57 +0530

    Password Generator

--- random-passwordgenerator.py
+++ random-passwordgenerator.py
@@ -13,9 +13,9 @@ def result():
     r=''
     for z in resultlist:
         r=r+z
-        return r
+    return r
     
-print('[GENERATE PASSWORD OF ANY LENGTH INCLUDING UPPER CASE, LOWER CASE AND SPECIAL CHARACTERS]')
+print('[GENERATE PASSWORD OF ANY LENGTH INCLUDING UPPER CASE, LOWER CASE, DIGIT AND SPECIAL CHARACTERS]')
 print('')
 resultlist=[]
 c=True
@@ -28,12 +28,15 @@ while c:
     print('')
     low=int(input('Enter no of Lower Case:'))
     print('')
+    dig=int(input('Enter number of Digits:'))
+    print('')
     sc=int(input('Enter no of Special Characters:'))
-    if char>=up+low+sc:
+    if char>=up+low+sc+dig:
         uplist=['Q', 'A', 'Z', 'X', 'S', 'W', 'E', 'D', 'C', 'V', 'F', 'R', 'T', 'G', 'B', 'N', 'H', 'Y', 'U', 'J', 'M', 'K', 'I', 'O', 'L', 'P']
         lowlist=['q', 'a', 'z', 'x', 's', 'w', 'e', 'd', 'c', 'v', 'f', 'r', 't', 'g', 'b', 'n', 'h', 'y', 'u', 'j', 'm', 'k', 'i', 'o', 'l', 'p']
+        digitlist=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         sclist=['!', '@', '#', '$', '%', '^', '&', '*','_', '-', '?', '.']
-        randomlist=['Q', 'A', 'Z', 'X', 'S', 'W', 'E', 'D', 'C', 'V', 'F', 'R', 'T', 'G', 'B', 'N', 'H', 'Y', 'U', 'J', 'M', 'K', 'I', 'O', 'L', 'P', 'q', 'a', 'z', 'x', 's', 'w', 'e', 'd', 'c', 'v', 'f', 'r', 't', 'g', 'b', 'n', 'h', 'y', 'u', 'j', 'm', 'k', 'i', 'o', 'l', 'p', '!', '@', '#', '$', '%', '^', '&', '*','_', '-', '?', '.']
+        randomlist=['Q', 'A', 'Z', 'X', 'S', 'W', 'E', 'D', 'C', 'V', 'F', 'R', 'T', 'G', 'B', 'N', 'H', 'Y', 'U', 'J', 'M', 'K', 'I', 'O', 'L', 'P', 'q', 'a', 'z', 'x', 's', 'w', 'e', 'd', 'c', 'v', 'f', 'r', 't', 'g', 'b', 'n', 'h', 'y', 'u', 'j', 'm', 'k', 'i', 'o', 'l', 'p', '!', '@', '#', '$', '%', '^', '&', '*','_', '-', '?', '.','0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         for x in range(up):
             a=random.choice(uplist)
             pos()
@@ -43,7 +46,10 @@ while c:
         for x in range(sc):
             a=random.choice(sclist)
             pos()
-        f=char-(up+low+sc)
+        for x in range(dig):
+            a=random.choice(digitlist)
+            pos()
+        f=char-(up+low+sc+dig)
         for x in range(f):
             a=random.choice(randomlist)
             pos()
